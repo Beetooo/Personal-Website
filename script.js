e4e8b5294af079c63ac33690715acf9f914afd8b
Beetooo <betosantospyne@gmail.com>
2024-06-15 00:16:39 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+//autotyping effect
+var typed=new Typed(".auto-type",{
+    strings: ["\"awesome\"","\"cool\"","\"a programmer\""],
+    typeSpeed:200,
+    backSpeed:150,
+    loop: true
+})
+
+//spotlight effect
+const spotlightEl = document.querySelector("#spotlight");
+let timeoutId;
+function handleMouseMove(event) {
+    const { clientX, clientY } = event;
+
+    timeoutId = setTimeout(() => {
+        requestAnimationFrame(() => {
+            spotlightEl.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px, #00000000 10px, #000000ee 350px)`;
+        });
+    }, 100);
+}
+document.addEventListener("mousemove", handleMouseMove);
+
+//nav bar fade when scrolling down
+let prevScrollPos = window.scrollY;
+window.onscroll = function() {
+    scrollFunction();
+};
+function scrollFunction() {
+    let currentScrollPos = window.scrollY;
+    
+    if (prevScrollPos > currentScrollPos||currentScrollPos<800) {
+        document.getElementById('main-menu').style.opacity = "1"; 
+    } else {
+        document.getElementById('main-menu').style.opacity = "0.3"; 
+        document.getElementById("main-menu").style.transition = "opacity 1s";
+    }
+    prevScrollPos = currentScrollPos;
+}
