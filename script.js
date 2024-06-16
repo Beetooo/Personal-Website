be18cfc1a8a34db95f45d42c78d2f1d3220c4716
Beetooo <betosantospyne@gmail.com>
2024-06-15 23:50:56 -0400

    added more items to work and made transition

--- script.js
+++ script.js
@@ -36,3 +36,37 @@ function scrollFunction() {
     }
     prevScrollPos = currentScrollPos;
 }
+
+//project slideshow effect
+document.addEventListener('DOMContentLoaded', () => {
+    const projects = document.querySelectorAll('.section');
+    let currentProject = 0;
+    const workSection = document.getElementById('work');
+    
+    window.addEventListener('wheel', (event) => {
+        if (workSection.getBoundingClientRect().top==0) {
+            if (event.deltaY > 0) {
+                // Scroll down
+                if (currentProject < projects.length - 1) {
+                    event.preventDefault();
+                    projects[currentProject].classList.remove('show');
+                    projects[currentProject].classList.add('hide');
+                    currentProject++;
+                    projects[currentProject].classList.remove('hide');
+                    projects[currentProject].classList.add('show');
+                }
+            } else {
+                // Scroll up
+                console.log(currentProject)
+                if (currentProject > 0) {
+                    event.preventDefault();
+                    projects[currentProject].classList.remove('show');
+                    projects[currentProject].classList.add('hide');
+                    currentProject--;
+                    projects[currentProject].classList.remove('hide');
+                    projects[currentProject].classList.add('show');
+                }
+            }
+        } 
+    }, { passive: false });
+});
